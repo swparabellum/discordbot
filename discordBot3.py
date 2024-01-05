962e972530aa02c11b35d858c835a69dac783ffd
swparabellum <swparabellum@gmail.com>
2024-01-05 14:30:41 +0900

    변경점 반영

--- discordBot3.py
+++ discordBot3.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
-
-client = commands.Bot(command_prefix='/')
+import discord
+client = discord.Client()
 
             #https://softvanilla.github.io/discordbot/discord_%EB%AA%85%EB%A0%B9%EC%96%B4_%EB%A7%8C%EB%93%A4%EA%B8%B0/
             #https://gihyeon.tistory.com/1
@@ -8,14 +8,15 @@ client = commands.Bot(command_prefix='/')
 TOKEN = 'MTE4Njg0NzUzOTU2ODMxNjUwNg.GvfZ7M.O7Yk8SDAgdiY2JOaGgJcMUWzCmi44dfpCeaH-w'
 CHANNEL_ID = '338962613775892491'
 
-class Weather():
-
-    def __init__(self):
-
+class Weather(discord.Client):
 
     @client.event
-    async def on_ready():
+    async def on_ready(self):
+        channel = self.get_channel(int(CHANNEL_ID))
         print("ready!")
 
     
+intents = discord.Intents.default()
+intents.message_content = True
+client = Weather(intents=intents)
 client.run(TOKEN)
